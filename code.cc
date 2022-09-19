8086c30a729fddcdcee25f70c5be4c9bfbc57758
Bruce Jin <85118325+ChengyuJin@users.noreply.github.com>
2022-09-18 22:46:44 -0400

    Create code.cc

---
+++ code.cc
@@ -0,0 +1,6 @@
+#include<iostream>
+
+int main() {
+  std::cout << "Due to the policy71 of University of Waterloo" << std::endl;
+  std::cout << "Please email me at c54jin@uwaterloo.ca for source code" << std::endl;
+}
